2e81d7f8e9178632e34e566eeeba53689c99b100
Moore <crimzero@hotmail.com>
2019-04-01 12:38:10 -0500

    no results error added

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -19,14 +20,14 @@ public class SearchController {
 
     public ArrayList<HashMap<String, String>> jobs;
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results")
+    @RequestMapping(value = "results", method = RequestMethod.GET)
     public String Search(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
         model.addAttribute("columns", ListController.columnChoices);
         if(searchType.equals("all"))
